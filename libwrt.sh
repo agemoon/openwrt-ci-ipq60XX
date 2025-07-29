32666beb4a8c1e79fbe9266cee6f303bdd5c9608
agemoon <41349219+agemoon@users.noreply.github.com>
2025-07-29 22:25:20 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,5 +1,5 @@
-rm -rf package/emortal/luci-app-athena-led
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+#rm -rf package/emortal/luci-app-athena-led
+#git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+#chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
